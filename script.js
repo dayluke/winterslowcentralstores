4b815df79a9f13f563512c82146870d31ed88af6
Luke Day <36641679+dayluke@users.noreply.github.com>
2019-08-14 00:49:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function navbarMenu() {
+  var nav = document.getElementById("nav");
+  var tabs = nav.getElementsByTagName("a");
+  var title = nav.getElementByClass("title");
+  
+  if (tabs[0].style.width === "100%") { // Menu is open, so close it.
+    for (var i = 0; i < tabs.length - 1; i++) {
+      tabs[i].style.width = null;
+      
+      if (window.innerWidth <= 1300) {
+        tabs[i].style.display = "none";
+      } else {
+        tabs[i].style.display = "initial";
+      }
+      title.style.float = "left";
+      title.style.textAlign = null;
+    }
+  } else { // Menu is closed, so open it.
+    for (var i = 0; i < tabs.length - 1; i++) {
+      tabs[i].style.width = "100%";
+      tabs[i].style.display = "block";
+    }
+    title.style.float = null;
+    title.style.textAlign = "center";
+  }
+}
